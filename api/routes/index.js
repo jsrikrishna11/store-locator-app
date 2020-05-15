38627e05cd82649c4bcb6e86809f76343ae4145c
Jujjavarapu Sri Krishna <41172782+jsrikrishna11@users.noreply.github.com>
2020-05-15 14:56:00 +0530

    Fixed info window issues.
    Had to change the react library that I was used to react-google-maps/api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,118 @@
 var express = require('express');
 var router = express.Router();
+var myParser = require("body-parser");
+const { Sequelize , DataTypes} = require('sequelize');
+const {Client, Status} = require("@googlemaps/google-maps-services-js")
+
 
 const key = "AIzaSyDcIChPg5D9yzbTfVUua6sCU22cEDYSMDM"
+
+/* Db settings */
+const sequelize = new Sequelize('Test', 'root', '1234', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+try{
+  sequelize.authenticate().then(()=> console.log("connection success!"));
+}
+catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+/* Models */
+
+const Store = sequelize.define('test',{
+  companyname:{
+    type: DataTypes.CHAR(5),
+    allowNull: true
+  },
+  companyid:{
+    type: DataTypes.NUMBER,
+    primaryKey: true
+  },
+  address1:{
+    type: DataTypes.CHAR(5),
+    allowNull: true
+  },
+  city:{
+    type: DataTypes.CHAR(5),
+    allowNull: true
+  },
+  state:{
+    type: DataTypes.CHAR(5),
+    allowNull: true
+  },
+  zip:{
+    type: DataTypes.CHAR(5),
+    allowNull: true
+  },
+  country:{
+    type: DataTypes.CHAR(20),
+    allowNull: true
+  },
+  lat:{
+    type: DataTypes.DECIMAL(8,4),
+    type: true
+  },
+  lng:{
+    type: DataTypes.DECIMAL(8,4),
+    allowNull: true
+  }
+},
+  {
+    freezeTableName: true,
+    createdAt: false,
+    updatedAt: false
+})
+
+console.log("store= "+ Store+ "is this undefined?")
+/*----------------------------------------------------*/
+
+/*Google maps setup*/
+
+const client = new Client({});
+// client.geocode({
+//   params:{
+//     address: 'GILBERT\'S WRITTEN WO'+" "+'72 CENTER SQUARE',
+//     key: key
+//   },
+//   timeout: 10000,
+// }).then((result)=>{
+//   if(result.data.status === Status.OK){
+//     result.data.results.forEach((location)=> console.log(location.geometry.location));
+//   }
+//   else console.log(result.data.error_message+ " is this undefined");
+// })
+// .catch((e)=>{
+//   console.log(e+" is this undefined?");
+// })
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  console.log(key)
+  let query_result;
+  Store.findOne().then((store, error)=>{
+    //write updation logic here!
+    
+    client.geocode({
+      params:{
+        address: store.companyname+" "+store.address1+" "+store.city+" "+store.state+" "+store.zip,
+        key: key
+      },
+      timeout: 10000,
+    }).then((result)=>{
+      if(result.data.status === Status.OK){
+        result.data.results.forEach((location)=> console.log(location.geometry.location));
+      }
+      else console.log(result.data.error_message+ " is this undefined");
+    })
+    .catch((e)=>{
+      console.log(e+" is this undefined?");
+    })
+  
 });
+});
+
 
 module.exports = router;
--- src/App.js
+++ src/App.js
@@ -1,55 +1,44 @@
-import React , {Component, useState, useEffect} from 'react';
+import React , {useState, useEffect} from 'react';
 import {usePosition } from 'use-position'
 import './App.css';
-import {
-  withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow, Circle
-} from 'react-google-maps'
 
-import Locations from './components/Location'
-import { wait } from '@testing-library/react';
+import {LoadScript, GoogleMap, Marker, InfoWindow} from "@react-google-maps/api"
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
+
+
+let ICON ="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+let infoID = 0;
+let markID = 0;
 /**------------------------------------------------------------------- */
 
-const MyMapComponent = withScriptjs(withGoogleMap((props) =>{
-  let id = 0;
-  let icon ="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-
+function MarkerWithWindow(props){
+  let [notWindowOpen, changeWinState ] = useState(false)
   return(
-  <GoogleMap 
-    defaultZoom={13}
-    defaultCenter={{ lat: props.latitude, lng: props.longitude}}
-  >
-    <Marker key={id++} position={{lat:props.latitude, lng: props.longitude}}
-      labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}>
-      <div>You!</div>
-    </Marker>
-    {props.isMarkerShown ? (
-      props.stores.map((store) => 
-       <Marker key={store.id} position={{ lat: store.geometry.location.lat, lng: store.geometry.location.lng}}
-       labelStyle={{backgroundColor: "blue", fontSize: "32px", padding: "16px"}} icon={icon} >
-         <div>{store.name}</div>
-       </Marker>
-    )) : 
-      (<></>)
-      }
-  </GoogleMap>
+    <React.Fragment key={markID++}>
+    <Marker key={props.store.id} position={props.store.geometry.location}
+          icon= {ICON}
+          onClick={()=> changeWinState(!notWindowOpen)}
+        >
+        </Marker>
+    {notWindowOpen && <InfoWindow position={props.position} key={props.store.place_id} 
+    onCloseClick={()=> changeWinState(false)}>
+      <div>
+        Name: {props.store.name}<br/>
+        Rating : {props.store.rating}<br/>
+        Address: {props.store.formatted_address}
+      </div>
+    </InfoWindow>}
+    </React.Fragment>
+  
   )
 }
-))
 
 
 function App() {
-
-  let [locationSet, setLocation] = useState(false)
   let {latitude, longitude} = usePosition()
   let [lat, setLat] = useState()
   let [lng, setLng] = useState() 
   let [stores, setStores] = useState(null)
-  let [changed, setChanged] = useState(false)
 
 
   useEffect(()=>{
@@ -67,49 +56,98 @@ function App() {
     const HEROKU = "https://cors-anywhere.herokuapp.com/";
     const GOOGLE = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
     console.log(radius);
-    let result;
     fetch(HEROKU+GOOGLE+`${storename}&location=${latitude},${longitude}&radius=${radius}&key=${process.env.REACT_APP_MAPBOX_TOKEN}`, 
     { headers:{'X-Requested-With':'application/x-yaml'}}).then(respose => respose.json() )
-    .then(data =>setStores(data.results) )
+    .then(data =>{
+      setStores(data.results)
+      console.log("changing stores values")
+    } )
   }
   
-  if(lat!= undefined || lng != undefined){
-  
-    if(stores != null){
+
+  if(lat!== undefined || lng !== undefined){
+  //if you found any stores!
+    if(stores !== null){
       console.log(stores)
       return (
         <>
-        found stores
-        {console.log(stores)}
-        <MyMapComponent latitude={lat} longitude={lng} isMarkerShown={true} stores={stores}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPBOX_TOKEN}&
-        v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-        />
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPBOX_TOKEN}>
+      <GoogleMap
+      key={123}
+        mapContainerStyle={{
+          width: '100vmax',
+          height: '400px'
+        }}
+        center={{
+          lat: lat,
+          lng: lng
+        }}
+        zoom={13}
+        
+      >
+        <Marker key={"retghnbvcxskmnvbc"}
+          position={
+            {
+              lat: lat,
+              lng: lng
+            }
+          }
+        >
+        </Marker>
+        {
+        stores.map((store, index) => {
+          return (
+        <React.Fragment key={index}>
+            <MarkerWithWindow key={store.id} position={store.geometry.location} store={store} />
+        </React.Fragment>
+        )}
+      )}
+      </GoogleMap>
+    </LoadScript>
+        
         <input type={"text"} placeholder={"Enter store here"} id={"name"}/> 
        <input type={"text"} placeholder={"Radius"} id={"radius"}/> 
        <button onClick={findStores} >Find stores</button>
-        </>
+    </>
       )
     }
   return (
-    <>
+    <React.Fragment key={infoID++}>
     {console.log(lat+" "+ lng)}
-      <div style={{ height: '100vh', width: '100%' }}>
-      <MyMapComponent latitude={lat} longitude={lng} isMarkerShown={false}
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_MAPBOX_TOKEN}&
-        v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-       <input type={"text"} placeholder={"Enter store here"} id={"name"}/> 
-       <input type={"text"} placeholder={"Radius"} id={"radius"}/> 
-       <button onClick={findStores} >Find stores</button>
-    </div>
-    </>
+     
+
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPBOX_TOKEN}>
+      <GoogleMap
+      
+        mapContainerStyle={{
+          width: '100vmax',
+          height: '400px'
+        }}
+        center={{
+          lat: lat,
+          lng: lng
+        }}
+        zoom={13}
+        key={"initContainer"}
+      >
+        <Marker
+          position={
+            {
+              lat: lat,
+              lng: lng
+            }
+          }
+          key={"initYou"}
+        >
+
+        </Marker>
+      </GoogleMap>
+    </LoadScript>
+    <input type={"text"} placeholder={"Enter store here"} id={"name"}/> 
+    <input type={"text"} placeholder={"Radius"} id={"radius"}/> 
+    <button onClick={findStores} id={"findStores"} >Find stores</button>
+
+    </React.Fragment>
   );
   }
   else{
@@ -117,5 +155,10 @@ function App() {
     
   }
 }
+
+
+
+
+
 
 export default App;
